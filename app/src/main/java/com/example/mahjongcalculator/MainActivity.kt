fca5e6160b3ad88c50b4f5e53dfb42d44a6945b3
Xiro234 <xiro234@gmail.com>
2022-03-09 15:03:28 -0500

    Added simple button that makes a toast.

--- app/src/main/java/com/example/mahjongcalculator/MainActivity.kt
+++ app/src/main/java/com/example/mahjongcalculator/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.mahjongcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newHand = findViewById<Button>(R.id.new_hand_button)
+
+        newHand.setOnClickListener {
+            Toast.makeText(this, "Click!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
